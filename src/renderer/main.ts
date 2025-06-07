b462d26e85a7104d6e1c7965e09442e3490c4115
Arthur Silva Dantas <arthur.dantas@ufms.br>
2025-06-07 09:05:00 -0300

    feat: remove implementation document and electron main file; add App.vue and integrate vue-toastification

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -6,9 +6,20 @@ import App from './App.vue';
 import router from './router';
 // @ts-ignore
 import { createPinia } from 'pinia';
+// @ts-ignore
+import Toast from 'vue-toastification';
+// @ts-ignore
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App);
 app.use(createPinia());
+
+app.use(Toast, {
+    position: 'top-right',
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnHover: true,
+});
 
 app.use(router);
 app.mount('#app');
